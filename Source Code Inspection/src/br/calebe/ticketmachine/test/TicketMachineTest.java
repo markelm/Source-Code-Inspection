22cb6bddaacaac6e700f87c3b0b57e1a5c67683d
markelm <markelmacedo@gmail.com>
2022-04-08 22:34:29 -0300

    TicketMachine tests

--- Source Code Inspection/src/br/calebe/ticketmachine/test/TicketMachineTest.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/test/TicketMachineTest.java
@@ -18,7 +18,7 @@ public class TicketMachineTest {
             int valor = 4;
             TicketMachine obj = new TicketMachine(valor);
             obj.inserir(10);
-            Assert.assertEquals(2, obj.getSaldo());
+            Assert.assertEquals(10, obj.getSaldo());
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -28,15 +28,54 @@ public class TicketMachineTest {
 
     }
 
+    @Test
     public void getSaldoTest(){
 
+        try {
+            int valor = 4;
+            TicketMachine obj = new TicketMachine(valor);
+            int[] arr = {2,4,5,10,50,100};
+            for (int i = 0; i < arr.length; i++){
+                obj.inserir(arr[i]);
+            }
+            Assert.assertEquals(171, obj.getSaldo());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
     }
 
+    @Test
     public void getTrocoTest(){
-
+        try{
+            int valor = 5;
+            TicketMachine obj = new TicketMachine(valor);
+            obj.inserir(10);
+            Assert.assertEquals(5, obj.getTroco());
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        
     }
 
+    @Test
     public void imprimir(){
+
+        try{
+            int valor = 10;
+            TicketMachine obj = new TicketMachine(valor);
+            obj.inserir(10);
+            System.out.println(obj.imprimir());
+            String result = "*****************\n";
+            result += "*** R$ 10,00 ****\n";
+            result += "*****************\n";
+            Assert.assertEquals(result, obj.imprimir());
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
         
     }
 }
